5a64aef7f0dbb0f05934e0b5e61d1ae036c16bcb
todo2 <mjhdevil@naver.com>
2023-01-30 03:45:06 +0900

    # EDIT - Add버튼 클릭시 TextFormField (글자 입력할때 사이즈 오류 - body: SingleChildScrollView 해결)

--- lib/View/main_calendar.dart
+++ lib/View/main_calendar.dart
@@ -3,7 +3,6 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:get/get.dart';
 
 class MainCalendar extends StatefulWidget {
-
   @override
   State<MainCalendar> createState() => _MainCalendarState();
 }
@@ -23,41 +22,45 @@ class _MainCalendarState extends State<MainCalendar> {
         title: Text('ToDo'),
         centerTitle: true,
       ),
-      body: TableCalendar(
-        locale: 'ko_KR',
+      body: SingleChildScrollView(
+        child: TableCalendar(
+          locale: 'ko_KR',
           focusedDay: focusedDay,
           firstDay: DateTime(2023),
           lastDay: DateTime(2024),
-        headerStyle: HeaderStyle(
-          titleCentered: true, // 제목 중앙 위치
-          formatButtonVisible: false, // 달력 크기 선택 옵션 없애기
+          headerStyle: HeaderStyle(
+            titleCentered: true, // 제목 중앙 위치
+            formatButtonVisible: false, // 달력 크기 선택 옵션 없애기
+          ),
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            // 선택된 날짜의 상태를 갱신합니다.
+            setState(() {
+              this.selectedDay = selectedDay;
+              this.focusedDay = focusedDay;
+            });
+          },
+          selectedDayPredicate: (DateTime day) {
+            // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+            return isSameDay(selectedDay, day);
+          },
         ),
-        onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-          // 선택된 날짜의 상태를 갱신합니다.
-          setState((){
-            this.selectedDay = selectedDay;
-            this.focusedDay = focusedDay;
-          });
-        },
-        selectedDayPredicate: (DateTime day) {
-          // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
-          return isSameDay(selectedDay, day);
-        },
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Get.bottomSheet(
-              Container(height: 150,
-                child: Column(
-                  children: [
-                    TextField()
-
-                  ],
-                ),
-              )
-          );
+        onPressed: () {
+          Get.bottomSheet(Column(
+            children: [
+              Expanded(
+                child: TextFormField(
+                    decoration: InputDecoration(
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                )),
+              ),
+            ],
+          ));
         },
-
         child: Icon(Icons.add),
       ),
     );
